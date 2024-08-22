eeb9df6b73c67cdb9f7b4c914575d121be91ee56
Pedro Cruz <pedro.cruz.lopez001@gmail.com>
2024-08-22 12:55:48 -0500

    Se agregan funcionalidades y se arreglan detalles graficos

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -17,4 +17,51 @@ function decrypt(text) {
         .replace(/ufat/g, "u");
 }
 
-console.log(encrypt("gato"));
+function handleEncrypt() {
+    let textarea = document.getElementById("input");
+    validateTextarea(textarea);
+    let text = textarea.value;
+    let textoEncriptado = encrypt(text);
+    addCssAttributes();
+    document.getElementById("result").innerText = textoEncriptado;
+}
+
+function handleDecrypt() {
+    let textarea = document.getElementById("input");
+    validateTextarea(textarea);
+    let text = textarea.value;
+    let textoDesencriptado = decrypt(text);
+    addCssAttributes();
+    document.getElementById("result").innerText = textoDesencriptado;
+
+}
+
+function copyToClipboard() {
+    let content = document.getElementById("result").innerText;
+    navigator.clipboard.writeText(content);
+    hideContent();
+}
+function hideContent(){
+    let copyMessage = document.getElementById('copyMessage');
+    copyMessage.classList.remove('hidden');
+    copyMessage.classList.add('visible');
+
+    setTimeout(() => {
+        copyMessage.classList.remove('visible');
+        copyMessage.classList.add('hidden');
+    }, 2000);
+}
+
+function addCssAttributes() {
+    let contentDiv = document.getElementById("result");
+    contentDiv.style.alignItems = "start";
+    contentDiv.style.justifyContent = "left";
+    contentDiv.style.textAlign = "left";
+}
+
+function validateTextarea(textarea) {
+    let value = textarea.value.toLowerCase();    
+    value = value.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+    value = value.replace(/[^a-z]/g, '');
+    textarea.value = value;
+}
